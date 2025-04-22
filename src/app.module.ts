fe951512134645a12281000e82c3a1a205c435e0
Jorge Personal <jorgem.olave@gmail.com>
2025-04-22 02:03:06 -0400

    Seccion 03
    clase hasta la seccion 03

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CarsController } from './cars/cars.controller';
+import { CarsService } from './cars/cars.service';
+
+@Module({
+  controllers: [CarsController],
+  providers: [CarsService],
+})
+export class AppModule {}
---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+  @Get() //GET ALL CARS
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+  @Get(':id') //GET CAR BY ID
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    console.log({ id });
+    return this.carsService.findOneById(id);
+  }
+  @Post() //POST ANY CAR
+  createCar(@Body() body: any) {
+    return body;
+  }
+  @Patch(':id') //(PATCH = UPDATE) CAR BY ID
+  updateCar(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    body: any,
+  ) {
+    return body;
+  }
+  @Delete(':id') //DELETE CAR BY ID
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id,
+    };
+  }
+}
