78cdac815d24621e86ff5d1aa3dca42b6d3e8e0f
Jorge Personal <jorgem.olave@gmail.com>
2025-04-23 03:38:10 -0400

    CRUD by NestJS CLI
    - Se utiliza el CLI de Nest para crear un CRUD API REST lo que permite tener todo el esqueleto de la API en minutos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsController } from './cars/cars.controller';
 import { CarsService } from './cars/cars.service';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
   controllers: [CarsController],
   providers: [CarsService],
+  imports: [BrandsModule],
 })
 export class AppModule {}
